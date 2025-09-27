72250eb7f4f745dc155cfe55e3b890c0e49b7c0e
aleksanderdimitrov <snowtromgs@gmail.com>
2025-09-27 17:00:00 +0300

    Decorative wave added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 import HeroSection from "@/components/Sections/Home/Hero";
+import DecorativeWave from "@/components/Sections/Home/DecorativeWave";
+
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
+      <DecorativeWave rotated={true}/>
     </main>
   );
 }
