0eb5fe7ce8035592d4aa3df85910187fbe49a697
aleksanderdimitrov <snowtromgs@gmail.com>
2025-09-27 17:22:42 +0300

    Platform 3-5 Section done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
-import HeroSection from "@/components/Sections/Home/Hero";
 import DecorativeWave from "@/components/Sections/Home/DecorativeWave";
+import HeroSection from "@/components/Sections/Home/Hero";
+import Platform3to5Section from "@/components/Sections/Home/Platform3to5";
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
       <DecorativeWave rotated={true}/>
+      <Platform3to5Section />
     </main>
   );
 }
