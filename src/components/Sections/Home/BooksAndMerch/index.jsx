6dc6ffebbd6d3f81878c21d034ecd992dd3bce19
aleksanderdimitrov <snowtromgs@gmail.com>
2025-10-23 02:07:34 +0300

    quick typo fix

--- src/components/Sections/Home/BooksAndMerch/index.jsx
+++ src/components/Sections/Home/BooksAndMerch/index.jsx
@@ -39,7 +39,7 @@ export default function BooksAndMerchSection() {
                 alt="Образователни книги"
                 fill
                 placeholder="blur"
-                blurDataURL="/favicon-192x192.png"
+                blurDataURL="/assets/kniga_reklama.png"
                 priority={false}
               />
             </div>
